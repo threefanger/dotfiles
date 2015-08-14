cf87b2db3d2bc4da8f91a411954fdc9647b8a576
vistamon82 <vistamon82@gmail.com>
2015-08-13 23:34:57 -0700

    wifi script rework

--- bin/wifi.sh
+++ bin/wifi.sh
@@ -1,10 +1,52 @@
 #! /bin/bash
 #muh amazign weefee script
+#
+# this script shows access point and current status in network name and icon.
+#
+# DISABLE WIFI DRIVER: ip link set wlan0 down
+# ENABLE WIFI DRIVER: ip link set wlan0 up
+# DISCONNECT FROM WIFI: netctl-auto disable-all
+# RECONNECT TO WIFI: netctl-auto enable-all
+#ICON=""
 
 while true;
 
 do
+	STATUS=$(wpa_cli status | grep wpa_state | sed 's/wpa_state=//')
+	ENCTYPE=$(wpa_cli status | grep key_mgmt | sed 's/key_mgmt=//')
+	NETWORK=$(essid)
 	SIGQUAL=$(iw wlp1s0 link | grep signal | sed 's/\tsignal: //')
-	echo $SIGQUAL
+#	echo $STATUS	# for DEBUG
+#	echo $ENCTYPE	# for DEBUG
+#	echo $NETWORK	# for DEBUG
+#	echo $SIGQUAL	# for DEBUG
+	if [ $STATUS == 'COMPLETED' ];
+		then
+			if [ $ENCTYPE == 'NONE' ];
+				then
+				ICON=''
+			elif [ $ENCTYPE == 'WPA2-PSK' ];
+				then
+				ICON=''
+			fi
+	elif [ $STATUS == 'INACTIVE' ];
+		then
+		NETWORK='Disconnected'
+		ICON=''
+	elif [ $STATUS == 'INTERFACE_DISABLED' ];
+		then
+		NETWORK='Disabled'
+		ICON=''
+        elif [ $STATUS == 'SCANNING' ];
+                then
+                NETWORK='Scanning...'
+                ICON=''
+        elif [ $STATUS == 'AUTHENTICATING' ];
+                then
+                NETWORK='Authenticating...'
+                ICON=''
+
+	fi
+	echo w$ICON $NETWORK $SIGQUAL
 	sleep 1
 done
